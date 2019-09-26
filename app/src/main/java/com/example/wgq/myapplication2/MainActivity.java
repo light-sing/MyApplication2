a73d420a2002a5ebb94fe0dd7cff908887fec19f
wangguoqiang <wangguoqiang@kuke.com>
2019-09-26 17:11:40 +0800

    xxx3

--- app/src/main/java/com/example/wgq/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/wgq/myapplication2/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tv;
     TextView tvw;
+    TextView tvw2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
