a8a677e4fb20b1f9897877d9ff00c8f67c33dd32
wangguoqiang <wangguoqiang@kuke.com>
2019-09-26 16:58:40 +0800

    xxx

--- app/src/main/java/com/example/wgq/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/wgq/myapplication2/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.wgq.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //11
+        //22
+        //
     }
 }
